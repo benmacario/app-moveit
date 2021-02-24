2e87223f1f6f904170b2f0ebf2e0ce9bffe86342
Brendon Machado <benmacario.s@gmail.com>
2021-02-24 20:50:29 -0300

    firt commit, create component ExperienceBar and styles

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: '58%' }}/>
+        <span className="current-experience" style={{left: '58%'}}>
+          300 xp
+        </span>
+      </div>
+      <span>688 xp</span>
+
+    </header>
+  )
+}
